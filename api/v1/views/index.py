8ccf63b38390bbbaa0be7f237f1bbd04423aa8f2
arsany199 <arsanykamal30@gmail.com>
2024-03-31 12:40:49 -0700

    index

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""my index"""
+"""index"""
 from api.v1.views import app_views
 from flask import jsonify
 from models import storage
@@ -10,19 +10,21 @@ from models.city import City
 from models.amenity import Amenity
 from models.review import Review
 
-
-@app_views.route('/status', strict_slashes=False)
-def returnstuff():
-    '''return stuff'''
-    return jsonify(status='OK')
+classes = {"users": "User", "places": "Place", "states": "State",
+           "cities": "City", "amenities": "Amenity",
+           "reviews": "Review"}
 
 
-@app_views.route('/stats', strict_slashes=False)
-def stuff():
-    '''JSON Responses'''
-    todos = {'states': State, 'users': User,
-            'amenities': Amenity, 'cities': City,
-            'places': Place, 'reviews': Review}
-    for key in todos:
-        todos[key] = storage.count(todos[key])
-    return jsonify(todos)
+@app_views.route('/status', methods=['GET'])
+def status():
+    ''' routes to status page '''
+    return jsonify({'status': 'OK'})
+
+
+@app_views.route('/stats', methods=['GET'])
+def count():
+    '''retrieves the number of each objects by type'''
+    count_dict = {}
+    for cls in classes:
+        count_dict[cls] = storage.count(classes[cls])
+    return jsonify(count_dict)
